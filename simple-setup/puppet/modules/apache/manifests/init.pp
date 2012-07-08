eaef5eda369cd5084dc92a0493c9b9ffdf3a9ef3
Jean-Marc Fontaine <jm@jmfontaine.net>
2012-07-08 18:13:58 +0200

    Changed single quotes to double quotes for more consistence

--- simple-setup/puppet/modules/apache/manifests/init.pp
+++ simple-setup/puppet/modules/apache/manifests/init.pp
@@ -1,28 +1,28 @@
 class apache {
-    package { 'apache' :
-        ensure => 'present',
-        name   => 'apache2',
+    package { "apache" :
+        ensure => "present",
+        name   => "apache2",
     }
 
-    file { 'apache.vhost' :
-        path    => '/etc/apache2/sites-available/default',
+    file { "apache.vhost" :
+        path    => "/etc/apache2/sites-available/default",
         ensure  => present,
-        content => template('apache/vhost.erb'),
-        owner   => 'root',
-        group   => 'root',
-        notify  => Exec['apache.reload'],
-        require => Package['apache']
+        content => template("apache/vhost.erb"),
+        owner   => "root",
+        group   => "root",
+        notify  => Exec["apache.reload"],
+        require => Package["apache"]
     }
 
-    service { 'apache' :
-        ensure  => 'running',
-        name    => 'apache2',
+    service { "apache" :
+        ensure  => "running",
+        name    => "apache2",
         enable  => true,
-        require => Package['apache'],
+        require => Package["apache"],
     }
 
-    exec { 'apache.reload' :
-      command     => "service apache2 reload",
-      refreshonly => true,
+    exec { "apache.reload" :
+        command     => "service apache2 reload",
+        refreshonly => true,
     }
 }
--- simple-setup/puppet/modules/system/manifests/init.pp
+++ simple-setup/puppet/modules/system/manifests/init.pp
@@ -12,20 +12,20 @@ class system {
     }
 
     # Update packages list before doing anything
-    exec { 'apt.update' :
-        command => 'apt-get update',
+    exec { "apt.update" :
+        command => "apt-get update",
     }
-    Exec['apt.update']->Package <| |>
+    Exec["apt.update"]->Package <| |>
 
     # Define message of the day
-    file { 'motd' :
-        path    => '/etc/motd.tail',
+    file { "motd" :
+        path    => "/etc/motd.tail",
         ensure  => present,
         content => $motd
     }
 
     # Install some useful packages
-    package { [ 'htop' ] :
+    package { [ "htop" ] :
         ensure => present,
     }
 }
